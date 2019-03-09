417534b74af1d8d0d571cd6e82d74365b62375e5
mdamek <marcind70@gmail.com>
2019-03-09 12:34:27 +0100

    Przeniosłem tworzenie nodow i elementow do Grid

--- MESMARCIN/Grid.cs
+++ MESMARCIN/Grid.cs
@@ -20,6 +20,40 @@ namespace MESMARCIN
             {
                 this.Nodes[i] = new Node();
             }
+
+            FullNodesAndElements();
+        }
+
+        private void FullNodesAndElements()
+        {
+            var deltaX = GlobalData.H / (GlobalData.mH - 1);
+            var deltaY = GlobalData.L / (GlobalData.mL - 1);
+            var k = 0;
+            while (k < GlobalData.NodesCount)
+            {
+                for (var i = 0; i < GlobalData.mL; i++)
+                {
+                    for (var j = 0; j < GlobalData.mH; j++)
+                    {
+                        Nodes[k].X = i * deltaX;
+                        Nodes[k].Y = j * deltaY;
+                        k++;
+                    }
+                }
+            }
+            var actualElement = 0;
+            for (var i = 1; i < GlobalData.ElementsCount + GlobalData.mL - 1; i++)
+            {
+                if (i % (GlobalData.mH) == 0)
+                {
+                    continue;
+                }
+                    Elements[actualElement].Id[0] = i - 1;
+                    Elements[actualElement].Id[1] = i;
+                    Elements[actualElement].Id[2] = GlobalData.mH + i;
+                    Elements[actualElement].Id[3] = GlobalData.mH + i - 1;
+                    actualElement++;
+            }
         }
 
         public void ShowNodesCornerValues()
